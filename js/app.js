cfdb68f1b3a6a03278c40509429bcddf3a7e19e0
Mostafa Mekawy <45906789+Mostafamekkawy3@users.noreply.github.com>
2019-08-12 18:54:23 +0200

    js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//cart 
+
+//toggle menu 
+	const cartinfo=document.getElementById("cart-info");
+	const cart =document.getElementById('cart');
+
+		cartinfo.addEventListener("click",()=>{
+			cart.classList.toggle("show-cart");
+
+		})
+
+
